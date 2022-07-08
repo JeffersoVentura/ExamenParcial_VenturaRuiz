4772269b559df5b2ead3c3f15b566c71e3b99bf5
JefferVentu <jvruiz62@gmail.com>
2022-07-08 18:00:20 -0500

    Creación del main

---
+++ jefferventu/src/main/java/com/mycompany/jefferventu/Main.java
@@ -0,0 +1,13 @@
+package com.mycompany.jefferventu;
+
+/**
+ *
+ * @author jvrui
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
