97bc611350c62e8932daae0962a0f73b2bfa0018
JefferVentu <jvruiz62@gmail.com>
2022-07-08 20:54:37 -0500

    Creación de la Clase Vuelo

---
+++ jefferventu/src/main/java/com/mycompany/jefferventu/Vuelo.java
@@ -0,0 +1,15 @@
+package com.mycompany.jefferventu;
+
+import java.util.Date;
+
+/**
+ *
+ * @author jvrui
+ */
+public class Vuelo {
+    private int numero;
+    private int horaSalida;
+    private int horaLlegada;
+    private Date fechaSalida;
+    private String destino;
+}
