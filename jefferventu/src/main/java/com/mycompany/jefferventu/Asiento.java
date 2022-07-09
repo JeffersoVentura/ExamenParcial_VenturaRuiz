f34ea5ccf6b0d87cb818edc20003d25ebdc89ea5
JefferVentu <jvruiz62@gmail.com>
2022-07-08 20:53:53 -0500

    Creación de la Clase Asiento

---
+++ jefferventu/src/main/java/com/mycompany/jefferventu/Asiento.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.jefferventu;
+
+/**
+ *
+ * @author jvrui
+ */
+public class Asiento {
+    private int numero;
+    private boolean estado;
+    private float precio;
+}
